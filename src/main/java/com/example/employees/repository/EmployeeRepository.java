0c4d3139271b295ad5cd4363b8406ff641314cc3
limzhengyang <zhengyanglim057@gmail.com>
2023-04-17 12:58:26 +0800

    new commit

--- src/main/java/com/example/employees/repository/EmployeeRepository.java
+++ src/main/java/com/example/employees/repository/EmployeeRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    @Query("SELECT a.firstName FROM employee WHERE a.empId = empId")
+    @Query("SELECT a.firstName FROM employee WHERE a.empId =: empId")
     public String getFirstNameById(@Param("id") Long empId);
 }
